ba5f40fabfa25038b1c07b2a14929a1567ceca84
DavidWolff218 <dcwolff218@gmail.com>
2023-06-13 17:49:42 -0500

    updated to functional fully, fixed props, and took away text color function and made it inline

--- src/components/allUsers.js
+++ src/components/allUsers.js
@@ -1,46 +1,40 @@
 import React from "react";
 
-const allUsers = () => {
+const allUsers = (props) => {
   
- const userColor = (userObj) => {
-    if (userObj.is_active === true) {
-      return <span><h5 className="userTrue">{userObj.username}</h5></span>
-    } else {
-      return <span><h5 className="userFalse">{userObj.username}</h5></span>
-    }
-  }
-  
- const renderAllUsers = () => {
-    
-    let allUsersArray = this.props.users.sort((a, b) => a.id - b.id);
-    
+  const renderAllUsers = () => {
+    let allUsersArray = props.users.sort((a, b) => a.id - b.id);
+
     return allUsersArray.map((userObj) => {
       return (
         <span className="eachUser" key={userObj.id}>
-          {this.userColor(userObj)}
+          {/* removed function call below and doing all in one line. tested and works, but noting in case problems later */}
+          {userObj.is_active ? (
+            <h5 className="userTrue">{userObj.username}</h5>
+          ) : (
+            <h5 className="userFalse">{userObj.username}</h5>
+          )}
         </span>
       );
     });
   };
 
- const renderBox = () => {
-    if (this.props.gameStarted === false) {
-      return null
+  const renderBox = () => {
+    if (props.gameStarted === false) {
+      return null;
     } else {
-    return <div className="allUsers">
-      <h3><span className="allUsersTitle">Players</span></h3>
-      <div className="allUsersList">{this.renderAllUsers()}</div>
-    </div>;
+      return (
+        <div className="allUsers">
+          <h3>
+            <span className="allUsersTitle">Players</span>
+          </h3>
+          <div className="allUsersList">{renderAllUsers()}</div>
+        </div>
+      );
+    }
   };
-}
 
- 
-    return (
-    <div>
-      {this.renderBox()}
-    </div>
-    )
-  
-}
+  return <div>{renderBox()}</div>;
+};
 
 export default allUsers;
