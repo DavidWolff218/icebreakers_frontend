ddd4c39dc2ac4bc99bad868b60ad3531b51a1de8
DavidWolff218 <dcwolff218@gmail.com>
2023-06-13 16:53:39 -0500

    changed navBar to functional, started props correction

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import icebreakersv8 from "../logo/icebreakersv8.png";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
-export class navBar extends Component {
-  logOutBtn = () => {
-    if (this.props.currentUser === this.props.host) {
+const navBar = (props) => {
+  const logOutBtn = () => {
+    if (props.currentUser === props.host) {
       return (
         <button className="LogoutBtn" onClick={this.props.endGameBtn}>
           End Game
@@ -17,33 +17,27 @@ export class navBar extends Component {
         </button>
       );
     }
-  };
-
-  render() {
-    return (
-      <Row className="nav-bar">
-        <Col className="col-3">{this.logOutBtn()}</Col>
-        <Col className="col-6 ">
-          {/* <div className="NavBarTitle"> */}
-          <img
-            className="nav-logo"
-            src={icebreakersv8}
-            alt="icebreakers logo"
-          />
-          {/* </div> */}
-        </Col>
-        <Col className="col-3 nav-bar-info">
-          <Row>
-            <Col><span className="nav-bar-info-room-bar">Room</span></Col>
-          </Row>
-          <Row>
-            <Col>{this.props.room}</Col>
-          </Row>
-        </Col>
-      </Row>
-    );
   }
-}
+  return (
+    <Row className="nav-bar">
+      <Col className="col-3">{logOutBtn()}</Col>
+      <Col className="col-6 ">
+        {/* <div className="NavBarTitle"> */}
+        <img className="nav-logo" src={icebreakersv8} alt="icebreakers logo" />
+        {/* </div> */}
+      </Col>
+      <Col className="col-3 nav-bar-info">
+        <Row>
+          <Col>
+            <span className="nav-bar-info-room-bar">Room</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>{this.props.room}</Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
 
 export default navBar;
-
