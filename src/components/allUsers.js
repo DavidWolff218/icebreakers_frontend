fbb436d2dfb417a732796ef07e226126771ad567
DavidWolff218 <dcwolff218@gmail.com>
2023-06-14 12:40:34 -0500

    test commit

--- src/components/allUsers.js
+++ src/components/allUsers.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const allUsers = (props) => {
-  
+console.log('test')
   const renderAllUsers = () => {
     let allUsersArray = props.users.sort((a, b) => a.id - b.id);
 
     return allUsersArray.map((userObj) => {
       return (
         <span className="eachUser" key={userObj.id}>
+          {/* not sure if this span needs to exist, will check later */}
           {/* removed function call below and doing all in one line. tested and works, but noting in case problems later */}
           {userObj.is_active ? (
             <h5 className="userTrue">{userObj.username}</h5>
