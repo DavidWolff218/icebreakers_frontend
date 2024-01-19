b37dd5ca60a07fb6876b5b0db59f606bfdd34b62
doodleman218 <dcwolff218@gmail.com>
2024-01-19 12:32:06 -0600

    cleaned up room, moved most of voting logic to its component

--- src/components/voting.js
+++ src/components/voting.js
@@ -1,6 +1,58 @@
 import React from "react";
 
 const voting = (props) => {
+
+  //These three functions are all part of the voting feature, will implement again in future
+  // moved here from room.js, may need to put some functionality back there, just cleaning room right now
+  
+  // const runTimer = () => {
+  //     const intervalID = setInterval(() => {
+  //       if (this.state.timerSeconds > 0) {
+  //         this.setState({
+  //           timerSeconds: this.state.timerSeconds - 1,
+  //         });
+  //         this.setState({
+  //           timerIntervalID: intervalID,
+  //         });
+  //       } else {
+  //         this.resetTimer();
+  //       }
+  //     }, 1000);
+  //   };
+
+  // const  resetTimer = () => {
+  //     clearInterval(this.state.timerIntervalID);
+  //     if (this.state.timerSeconds === 0) {
+  //       this.timerSelect();
+  //       this.setState({
+  //         // timerRunning: false,
+  //         timerSeconds: 20,
+  //       });
+  //     } else {
+  //       this.setState({
+  //         // timerRunning: false,
+  //         timerSeconds: 20,
+  //       });
+  //     }
+  //   };
+
+  // const  timerSelect = () => {
+  //     console.log("timer");
+  //     const reqObj = {
+  //       method: "PATCH",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         user: {
+  //           room: this.props.match.params.id,
+  //           currentPlayer: this.state.currentPlayer,
+  //         },
+  //       }),
+  //     };
+  //     fetch(`http://localhost:3000/users/voting_timer/foo`, reqObj);
+  //   };
+
   // componentDidMount() {
   //   this.props.runTimer();
   // }
